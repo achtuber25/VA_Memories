c33d08ad897cebde7d669ffc58e4c15bfdfa30bb
achtuber <achtuber@gmail.com>
2022-08-19 17:43:05 +0530

    axa

--- loginAuth.js
+++ loginAuth.js
@@ -1,4 +1,6 @@
+console.log(localStorage.getItem("auth") );
 if (localStorage.getItem("auth") == 0) {
+    console.log(localStorage.getItem("auth") );
     alert("login first")
     window.location.href = "index2.html"
 }
