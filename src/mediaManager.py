9764a84ed0646f7813cf0a823b38775b44756d77
achtuber <achtuber@gmail.com>
2023-01-31 12:08:36 +0530

    d

--- src/mediaManager.py
+++ src/mediaManager.py
@@ -1,22 +1,25 @@
 import os
+import glob,os
+
 
 
 # Function to rename multiple files
 def main():
     i = 0
     path = "D:\\BGMI\\VA_Memories\\src\\videos\\"
-    for filename in os.listdir(path):
+    lst_files = glob.glob(path+"*.mp4")
+    lst_files.sort(key=os.path.getmtime)
+    print(lst_files)
+    for filename in lst_files:
+        filename = filename.split("\\")[-1]
         print(filename)
         my_dest = "vA" + str(i) + ".mp4"
         my_source = path + filename
         my_dest = path + my_dest
-    # rename() function will
-    # rename all the files
         os.rename(my_source, my_dest)
         i += 1
 
 
 # Driver Code
 if __name__ == '__main__':
-    # Calling main() function
     main()
